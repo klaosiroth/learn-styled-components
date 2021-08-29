dee461331746231b464e73179fa1c041c4d61711
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-08-29 22:22:29 +0700

    Animations.

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,24 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Button from './Button';
 import Icon from './Icon';
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
 
 const Section = styled.section`
   background-color: #aac9f0;
@@ -8,10 +26,15 @@ const Section = styled.section`
   justify-content: center;
   flex-direction: column;
   border-radius: 15px;
+
+  .animation {
+    animation: 2s ${rotate} linear infinite
+  }
 `
 
 const Title = styled.h1`
   color: #aaecf0;
+  animation: 3s ${fadeIn} ease-in;
 `;
 
 const SubTitle = styled(Title)`
@@ -32,6 +55,7 @@ const ReversedTitle = (props) => (
 const Content = () => {
   return (
     <Section>
+      <div className="animation">circle</div>
       <Title>💅🏻 Section</Title>
       <SubTitle>I am a subtitle</SubTitle>
       <Button primary text="me first" />
