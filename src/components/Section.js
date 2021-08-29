9e69ce7b3df1d0f8e4e4a7991404ac6d6dea5234
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-08-29 20:10:30 +0700

    Separating Components and passing props.

---
+++ src/components/Section.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const StyledSection  = styled.section`
+  background-color: #ffffff;
+  border: solid 3px grey;
+  margin: 4px;
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: left;
+  font-size: 40px;
+  color: #ff6961;
+`;
+
+const WeekdayTitle  = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 50px;
+  border-right: solid 3px lightgrey;
+`;
+
+const Section = ({ text }) => {
+  return (
+    <StyledSection>
+      <WeekdayTitle>{text}</WeekdayTitle>
+    </StyledSection>
+  )
+}
+
+export default Section;
