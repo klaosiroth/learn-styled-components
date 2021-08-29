67f79627aa3942fae1aa52b17970992df2f45358
Natthaphon Wichianphong <klaosiroth@gmail.com>
2021-08-29 20:04:28 +0700

    Setting up from scratch.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #b19cd9;
+`
+
+const Section = styled.div`
+  background-color: #ffffff;
+  border: solid 3px grey;
+  margin: 4px;
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: left;
+  font-size: 40px;
+  color: #ff6961;
+`;
+
+const WeekdayTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50px;
+  border-right: solid 3px lightgrey;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title>Progress Tracker</Title>
+      <div>
+        <Section>
+          <WeekdayTitle>M</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>T</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>W</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>T</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>F</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>S</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>S</WeekdayTitle>
+        </Section>
+      </div>
+    </>
   );
 }
 
